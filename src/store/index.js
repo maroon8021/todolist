7c6b1611fa9b52cf1e8990283da8d9a6f3b9df81
Tatsuhiko Mitsui <Tatsuhiko_Mitsui@Tatsuhiko-no-MacBook-Pro.local>
2018-09-29 16:58:53 +0900

    refs #180929 Prepared new modules

---
+++ src/store/index.js
@@ -0,0 +1,39 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+const state = {
+  mode: ''
+};
+
+//Mutaions is synchronous only methods to change state values.
+const mutations = {
+  commitModeToDev(state,payload) {
+    state.mode = 'devlopment';
+  },
+  commitModeToPro(state,payload) {
+    state.mode = 'production'; 
+  }
+};
+
+//Actions is methods includes asynchronous processes.
+//and commit mutations.
+const actions = {
+  getSomething({commit},payload) {
+    const num1 = 1;
+    const num = 2;
+  },
+  updateModeDev({commit},payload) {
+    commit('commitModeToDev',payload);
+  },
+  updateModePro({commit},payload) {
+    commit('commitModeToPro',payload);
+  }
+};
+
+export default new Vuex.Store({
+  state: state,
+  mutations: mutations,
+  actions: actions
+});
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: [
+    'babel-polyfill',
+    path.resolve('src', 'index.js')
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dest/')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(css|sass|scss)$/,
+        loader: 'sass-loader',
+        options: {
+          outputStyle: 'expanded',
+          sourceMap: true,
+        },
+      },
+      {
+        test: /\.(jpg|png|json|svg)$/,
+        loaders: 'url-loader'
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.js','json','jsx','.vue'],
+    alias: {
+      vue$: 'vue/dist/vue.esm.js',
+    },
+  },
+  devServer: {
+    contentBase: 'dest',
+  },
+
+};
